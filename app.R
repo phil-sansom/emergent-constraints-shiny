8f4532b22f6b6cd6f3b5ab3a588f912f599aea45
Phil Sansom <p.g.sansom@exeter.ac.uk>
2019-02-13 16:56:06 +0000

    Added missing library statement.

--- app.R
+++ app.R
@@ -5,6 +5,7 @@
 ## Preamble
 library(MASS)
 library(rstan)
+library(shiny)
 options(mc.cores = 4)             ## Set the number of CPU cores to use
 rstan_options(auto_write = TRUE)  ## Prevent STAN recompiling unnecessarily
 model = stan_model("model.stan")  ## Load STAN model
